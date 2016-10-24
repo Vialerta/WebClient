044fdfdb6b479848dfb874aafa539de9e7e689ab
Erik Perin <erikperin.photo@gmail.com>
2016-10-24 16:41:53 -0200

    Creating Footer

--- views/templates/modules/footer.php
+++ views/templates/modules/footer.php
@@ -1,9 +1,34 @@
 </div>
 <footer>
-<div class="block">
+<div class="block contrast">
   <div class="container container-center">
-  Licensed under GPLv3
+
+
+<div  class="grid">
+  <div class="width-medium-1-3">
+<img width="80" src="assets/img/gplv3.svg" />
+  <p>
+  GNU General Public License
+  </p>
   </div>
+  <div class="width-medium-1-3">
+  <h3 >It's Lost? <small>Visit our</small> </h3> <button  class="button button-large width-medium-1-2"><i class="icon-life-bouy"></i> Support</button>
+  </div>
+
+  <div class="width-medium-1-3">
+  <h3>Stay connected</h3>
+  <a href="" class="button github"><i class="icon-github"></i></a>
+  <a href="" class="button facebook"><i class="icon-facebook"></i></a>
+  <a href="" class="button twitter"><i class="icon-twitter"></i></a>
+    <a href="" class="button linkedin"><i class="icon-linkedin"></i></a>
+  </div>
+
+</div>
+
+  </div>
+</div>
+<div class="dark-grey text-center">
+Web Client - Powered by Vialerta Core - GNU General Public License
 </div>
 </footer>
   </body>
